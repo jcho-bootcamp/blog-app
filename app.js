a9ebb87a1111084b1b275ce2b7487c991dee2d70
June Hyok Cho <jcho0627@gmail.com>
2019-04-21 22:57:27 +0200

    create route and form input in new template

--- app.js
+++ app.js
@@ -31,12 +31,29 @@ app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", (req, res) => {
   Blog.find({}, (err, blogs) => {
     if (err) {
       console.log("ERROR");
     } else {
       res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+// NEW ROUTE
+app.get("/blogs/new", (req, res) => {
+  res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", (req, res) => {
+  Blog.create(req.body.blog, (err, newBlog) => {
+    if (err) {
+      res.render("/new");
+    } else {
+      res.redirect("/blogs");
     }
   });
 });
