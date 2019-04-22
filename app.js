665035b41189bef9ca2694223423a2bb25686eeb
June Hyok Cho <jcho0627@gmail.com>
2019-04-22 12:07:59 +0200

    show route added

--- app.js
+++ app.js
@@ -58,6 +58,15 @@ app.post("/blogs", (req, res) => {
   });
 });
 
-
+// SHOW ROUTE
+app.get("/blogs/:id", (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", { blog: foundBlog });
+    }
+  });
+});
 
 app.listen(3000, () => console.log("Server is Listening on Port : 3000"));
