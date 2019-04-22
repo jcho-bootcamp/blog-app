70baac80e021f8b62af25291c732c2adcb025190
June Hyok Cho <jcho0627@gmail.com>
2019-04-22 15:29:04 +0200

    delete route and edit / delete links added to show

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
@@ -7,8 +8,10 @@ const app = express();
 mongoose.connect("mongodb://localhost/blog-app", { useNewUrlParser: true });
 
 app.set("view engine", "ejs");
+mongoose.set('useFindAndModify', false);
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // MONGOOSE / MODEL CONFIG
 let blogSchema = new mongoose.Schema({
@@ -65,6 +68,41 @@ app.get("/blogs/:id", (req, res) => {
       res.redirect("/blogs");
     } else {
       res.render("show", { blog: foundBlog });
+    }
+  });
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", { blog: foundBlog });
+    }
+  });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", (req, res) => {
+  // Destroy blog
+  Blog.findByIdAndRemove(req.params.id, (err) => {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      // Redirect
+      res.redirect("/blogs");
     }
   });
 });
